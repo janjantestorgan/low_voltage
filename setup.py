e0b539b0f1054fab5a3375e173b4b980d5559d2d
Candan Dozen <candan.dozen@cern.ch>
2022-09-19 20:06:26 +0200

    test4

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
-
-
 setup(
     name="low_voltage",
     version="1.0.0",
     packages=find_packages(),
-    include_package_data=True,
+    #include_package_data=True
     zip_safe=False,
-    #python_requires='>=3.7',
-    #install_requires=[
-	#'influxdb-client',
-	#'paho-mqtt',
-	#'pyserial',
-    #],
-)
+
+
+
+
+)    
+    
+    
+   
