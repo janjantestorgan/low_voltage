de999b0fb5b1e613bb30c4aca87761cb22f320aa
Candan Dozen <candan.dozen@cern.ch>
2022-09-19 18:54:49 +0200

    daqmx error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="low_voltage",
     version="1.0.0",
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
     zip_safe=False,
     #python_requires='>=3.7',
     #install_requires=[
